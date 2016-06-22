ae218816f810b83ca1922aa4438d3db5f2e362ba
Jeff Mahoney <jeffm@suse.com>
2016-06-22 18:54:39 -0400

    btrfs: root->fs_info cleanup, may_wait_transction/wait_current_trans/btrfs_throttle
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- fs/btrfs/relocation.c
+++ fs/btrfs/relocation.c
@@ -3115,6 +3115,7 @@ int setup_extent_mapping(struct inode *inode, u64 start, u64 end,
 static int relocate_file_extent_cluster(struct inode *inode,
 					struct file_extent_cluster *cluster)
 {
+	struct btrfs_fs_info *fs_info = btrfs_sb(inode->i_sb);
 	u64 page_start;
 	u64 page_end;
 	u64 offset = BTRFS_I(inode)->index_cnt;
@@ -3210,7 +3211,7 @@ static int relocate_file_extent_cluster(struct inode *inode,
 
 		index++;
 		balance_dirty_pages_ratelimited(inode->i_mapping);
-		btrfs_throttle(BTRFS_I(inode)->root);
+		btrfs_throttle(fs_info);
 	}
 	WARN_ON(nr != cluster->nr);
 out:
