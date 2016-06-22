e5dfc0d65b3b3efe317c6060f47438d1919518b8
Jeff Mahoney <jeffm@suse.com>
2016-06-22 18:54:52 -0400

    btrfs: root->fs_info cleanup, __push_leaf_right
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- fs/btrfs/ctree.c
+++ fs/btrfs/ctree.c
@@ -3633,14 +3633,13 @@ noinline int btrfs_leaf_free_space(struct btrfs_fs_info *fs_info,
  * right.  We'll push up to and including min_slot, but no lower
  */
 static noinline int __push_leaf_right(struct btrfs_trans_handle *trans,
-				      struct btrfs_root *root,
+				      struct btrfs_fs_info *fs_info,
 				      struct btrfs_path *path,
 				      int data_size, int empty,
 				      struct extent_buffer *right,
 				      int free_space, u32 left_nritems,
 				      u32 min_slot)
 {
-	struct btrfs_fs_info *fs_info = root->fs_info;
 	struct extent_buffer *left = path->nodes[0];
 	struct extent_buffer *upper = path->nodes[1];
 	struct btrfs_map_token token;
@@ -3844,7 +3843,7 @@ static int push_leaf_right(struct btrfs_trans_handle *trans, struct btrfs_root
 		return 0;
 	}
 
-	return __push_leaf_right(trans, root, path, min_data_size, empty,
+	return __push_leaf_right(trans, fs_info, path, min_data_size, empty,
 				right, free_space, left_nritems, min_slot);
 out_unlock:
 	btrfs_tree_unlock(right);
