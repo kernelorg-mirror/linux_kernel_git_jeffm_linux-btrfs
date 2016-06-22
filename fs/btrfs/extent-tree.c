98c52b483458d6ae27a460877a96ba346bd89443
Jeff Mahoney <jeffm@suse.com>
2016-06-22 18:56:09 -0400

    btrfs: root->fs_info cleanup, btrfs_check_trunc_cache_free_space
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -3401,7 +3401,7 @@ again:
 	WARN_ON(ret);
 
 	if (i_size_read(inode) > 0) {
-		ret = btrfs_check_trunc_cache_free_space(root,
+		ret = btrfs_check_trunc_cache_free_space(fs_info,
 					&fs_info->global_block_rsv);
 		if (ret)
 			goto out_put;
