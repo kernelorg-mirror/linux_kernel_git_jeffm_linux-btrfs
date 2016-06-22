0fd88f18f991a1a2d65bb5369050199a85cbf8f9
Jeff Mahoney <jeffm@suse.com>
2016-06-22 18:54:26 -0400

    btrfs: root->fs_info cleanup, btrfs_exclude_logged_extents
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -2549,7 +2549,7 @@ int btrfs_pin_extent(struct btrfs_root *root,
 		     u64 bytenr, u64 num, int reserved);
 int btrfs_pin_extent_for_log_replay(struct btrfs_root *root,
 				    u64 bytenr, u64 num_bytes);
-int btrfs_exclude_logged_extents(struct btrfs_root *root,
+int btrfs_exclude_logged_extents(struct btrfs_fs_info *fs_info,
 				 struct extent_buffer *eb);
 int btrfs_cross_ref_exist(struct btrfs_trans_handle *trans,
 			  struct btrfs_root *root,
